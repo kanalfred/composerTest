8c01aedc9116b2f74f51f4644382e6c608158196
Alfred <kanalfred@gmail.com>
2016-02-17 14:43:19 -0800

    first commit

---
+++ src/Test.php
@@ -0,0 +1,15 @@
+<?php
+namespace kanalfred;
+//namespace kanalfred;
+/**
+ * A terribly useful class
+ *
+ */
+class Test
+{
+    public static function hello()
+    {
+        return 'Testing printing HELLO HELLO !!!!!';
+    }
+}
+
